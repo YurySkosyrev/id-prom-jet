e7136711da7607b8f7b6968613e89a9b2546c5f6
YurySkosyrev <skosyrev-87@mail.ru>
2023-11-19 21:48:39 +0300

    Start adding common entities

---
+++ common/src/main/java/com/itprom/jet/common/bean/Airport.java
@@ -0,0 +1,30 @@
+package com.itprom.jet.common.bean;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Airport {
+    private String name;
+    private List<String> boards;
+    private int x;
+    private int y;
+
+    private void addBoard(String board) {
+        int ind = boards.indexOf(board);
+        if (ind >= 0) {
+            boards.set(ind, board);
+        } else {
+            boards.add(board);
+        }
+    }
+
+    private void removeBoard(String board) {
+        boards.remove(board);
+    }
+}
