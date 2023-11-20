4f23324ae58b02959a2d1dad2b85efd46b17ecc4
YurySkosyrev <skosyrev-87@mail.ru>
2023-11-20 17:27:47 +0300

    Add AirportStateMessage

---
+++ common/src/main/java/com/itprom/jet/common/messages/AirportStateMessage.java
@@ -0,0 +1,18 @@
+package com.itprom.jet.common.messages;
+
+import com.itprom.jet.common.bean.Airport;
+import com.itprom.jet.common.bean.Source;
+import com.itprom.jet.common.bean.Type;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AirportStateMessage extends Message{
+    private Airport airport;
+
+    public AirportStateMessage(Airport airport) {
+        this.source = Source.AIRPORT;
+        this.type = Type.STATE;
+    }
+}
